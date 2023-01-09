47baba2808b0e6e76bb8be600d9cb131b0f29663
TimurMP <titimurmamyshev@gmail.commur>
2023-01-09 23:32:35 +0200

    1

--- homework.js
+++ homework.js
@@ -15,13 +15,6 @@ function bubbleSort(arr) {
     }
 }
 
-function printArray(arr) {
-    for (let i in arr) {
-        console.log(arr[i])
-    }
-}
-
-
 
 console.log("=================PART II ================")
 const numbers2 = [25, 15, 11, 1, 7, 2, 3, 5, 4, 1, 11]
@@ -36,13 +29,13 @@ printArray(strings2);
 
 function bubbleSort2(arr, comparator) {
     for (let i = 0; i < arr.length - 1; i++) {
-            comparator(i, arr)
+        comparator(i, arr)
     }
 
 }
 
 function numComparator(i, arr) {
-    for (let j = 0; j < arr.length - 1; j++){
+    for (let j = 0; j < arr.length - 1; j++) {
         if (arr[j] > arr[j + 1]) {
             let t = arr[j];
             arr[j] = arr[j + 1];
@@ -52,12 +45,19 @@ function numComparator(i, arr) {
 }
 
 function stringLengthComparator(i, arr) {
-    for (let j = 0; j < arr.length - 1; j++){
+    for (let j = 0; j < arr.length - 1; j++) {
         if (arr[j].length > arr[j + 1].length) {
             let t = arr[j];
             arr[j] = arr[j + 1];
             arr[j + 1] = t;
         }
+    }
+}
+
+
+function printArray(arr) {
+    for (let i in arr) {
+        console.log(arr[i])
     }
 }
 
